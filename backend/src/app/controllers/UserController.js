f64aa708893eef86d3fa34b2a6050692a32a3b0c
williamwjd <william.jose.wjd@gmail.com>
2020-06-15 08:03:42 -0300

    validando dados do usuário com yup

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,7 +1,18 @@
+import * as Yup from "yup";
 import User from "../models/User";
 
 class UserController {
   async store(req, res) {
+    const Schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await Schema.isValid(req.body))) {
+      return res.status(401).json({ errors: "Schema is invalid" });
+    }
+
     const { name, email, password } = req.body;
     const user = await User.create({
       name,

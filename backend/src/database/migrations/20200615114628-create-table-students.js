1cadf7ba429ffc0c34f9cc817eb57ff76852e240
williamwjd <william.jose.wjd@gmail.com>
2020-06-15 08:55:51 -0300

    criando migration students

---
+++ backend/src/database/migrations/20200615114628-create-table-students.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("students", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      peso: {
+        type: Sequelize.REAL(11, 12),
+        allowNull: false,
+      },
+      altura: {
+        type: Sequelize.REAL(11, 12),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable("students");
+  },
+};

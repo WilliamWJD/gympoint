de4d167f244b6f927e21059038da27cdcd40e618
williamwjd <william.jose.wjd@gmail.com>
2020-06-16 08:55:29 -0300

    realiza checkin e lista checkin

--- backend/src/app/controllers/CheckinController.js
+++ backend/src/app/controllers/CheckinController.js
@@ -1,3 +1,5 @@
+import { startOfWeek, endOfWeek } from "date-fns";
+import { Op } from "sequelize";
 import Checkin from "../models/Checkin";
 // import Student from "../models/Student";
 
@@ -5,7 +7,7 @@ class CheckinController {
   async index(req, res) {
     const check = await Checkin.findAll({
       where: {
-        id: req.params.id,
+        student_id: req.params.id,
       },
     });
 
@@ -14,9 +16,31 @@ class CheckinController {
 
   async store(req, res) {
     const { id } = req.params;
+
+    const startWeek = startOfWeek(new Date());
+    const endWeek = endOfWeek(new Date());
+
+    // VERIFICA QUANTIDADE DE CHCKINS DO USUÁRIOS EM UMA SEMANA
+    const quantityCheckin = await Checkin.findAll({
+      where: {
+        student_id: id,
+        created_at: {
+          [Op.between]: [startWeek, endWeek],
+        },
+      },
+    });
+
+    if (quantityCheckin.length >= 5) {
+      return res.status(401).json({
+        errors:
+          "The maximum number of checkin allowed per week has been exceeded",
+      });
+    }
+
     const check = await Checkin.create({
       student_id: id,
     });
+
     return res.json(check);
   }
 }
